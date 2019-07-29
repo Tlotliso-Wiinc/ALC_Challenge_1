71274427a8b911373e179ca642eba32ccacdadb4
Tlotliso Mafantiri <michaelmafantiri@gmail.com>
2019-07-29 10:22:30 +0200

    Add all initial files

---
+++ app/src/main/java/tlotliso/Challenge/ActivityB.java
@@ -0,0 +1,42 @@
+package tlotliso.Challenge;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ActivityB extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://andela.com/alc");
+    }
+
+}
